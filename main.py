754db5c28c84f822dcbd84b645e6b72a68bf12d4
AbbyH2025 <william.hendrix2025@gbwl.org>
2024-10-01 15:11:41 -0400

    data selection with indecies, slicing

--- main.py
+++ main.py
@@ -18,7 +18,33 @@ print(data)
 print("Seniors are in grade ", grades['senior'])
 
 # can also create a series from a dictorany 
-cockiesDict = { 'double chocolate': 5, 'chocolate chip': 8.5, 'oatmeal raisin': 9, 'snickerdoodle': 10,  'dirt': 1 }
+cookiesDict = { 'double chocolate': 5, 'chocolate chip': 8.5, 'oatmeal raisin': 9, 'snickerdoodle': 10,  'dirt': 1 }
 cookies = pd.Series(cookiesDict)
 print(cookies)
 print("rating of dirt cookies: ", cookies['dirt'])
+
+#Dataframes is like a 2d array or specialized dictionary 
+cookiesDF = pd.DataFrame(cookies, columns=['rating'])
+print(cookiesDF)
+
+#add a column to our dataframe 
+cookiesDF['allergens'] = [True, True, True, True, False]
+print(cookiesDF)
+
+cookiesDF['Sweetness'] = { 'double chocolate': 10, 'chocolate chip': 9, 'oatmeal raisin': 7, 'snickerdoodle': 10,  'dirt': 1, 'birthday cake':1000}
+print(cookiesDF)
+
+data = pd.Series(['a','c','e'], index=[1,3,5])
+
+#indexing 
+print(data[3])
+
+#slcing
+print(data[3:5]) #not getting expected 
+
+#instead use LOC to get a slice that uses explictit indecies 
+#inclusive at index 5
+print(data.loc[3:5])
+
+#not as common, iLOC uses implicit indeices
+print(data.iloc[0:1]) #doesnt include second
