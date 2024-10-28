ba720ec3172c792f18c43ac57ded6b4a40424bce
AbbyH2025 <william.hendrix2025@gbwl.org>
2024-10-28 08:55:59 -0400

    california girl were undefeatable whatever

---
+++ cities.py
@@ -0,0 +1,27 @@
+import pandas as pd 
+from matplotlib import pyplot as plt 
+import numpy as np
+
+#load our CSV into a dataframe 
+cities = pd.read_csv('california_cities.csv')
+print(cities.info())
+
+lat = cities['latd'] #y values cos 
+lon = cities['longd'] #x values syn
+pop = cities['population_total']
+area = cities['area_total_sq_mi']
+
+#scatterplot 
+plt.scatter(lon, lat, label=None, c=np.log10(pop), cmap='RdPu', s=area, linewidth=0, alpha=0.5)
+plt.axis('equal')
+plt.xlabel('longitude')
+plt.ylabel('latitude')
+plt.colorbar(label='log$_{10}$(population)')
+plt.clim(3, 7)
+plt.title('CA Cities: Area and population')
+for area in [100, 300, 500]:
+    plt.scatter([], [], c='k', alpha=0.3, s=area,
+                label=str(area) + ' km$^2$')
+plt.legend(scatterpoints=1, frameon=False, labelspacing=1, title='City Area')
+plt.savefig('cities.png')
+plt.close()
